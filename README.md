a4a8ff28438269092a4cdee304053f6e8afaa1ad
beckermr <becker.mr@gmail.com>
2026-09-05 02:05:46 -0500

    feat: first draft of action

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # update-feedstock-version
-GitHub action to update the version of a feedstock.
+
+GitHub Action to update the version of a feedstock.
+
+## Usage
+
+Add a GitHub Actions workflow file like this one:
+
+```yaml
+name: update feedstock version
+on:
+  workflow_dispatch:
+    inputs:
+      version:
+        description: 'the new version'
+        required: true
+        type: string
+
+jobs:
+  update-feedstock-version:
+    runs-on: ubuntu-latest
+    name: update-feedstock-version
+    steps:
+      - name: run
+        uses: beckermr/update-feedstock-version@main
+        with:
+          feedstock: <name of feedstock>-feedstock
+          version: ${{ inputs.version }}
+          # A GitHub personal access token is required
+          github-token: ${{ secrets.GITHUB_PAT }}
+          automerge: true
+```
+
+Then you can trigger the version update by dispatching the workflow in the UI. It is also possible to trigger the workflow on GitHub release events.
+
+See the [action.yml](action.yml) for details on possible inputs and options.
---
+++ update_version.py
@@ -0,0 +1,95 @@
+import logging
+import pprint
+import sys
+from pathlib import Path
+
+from conda_forge_feedstock_ops.rerender import rerender as cf_feedstock_ops_rerender
+
+LOGGER = logging.getLogger(__name__)
+
+
+def main(feedstock_name, new_version):
+    # these imports are guarded here in this function since the
+    # conda_forge_tick package will hide sensitive env vars
+    import conda_forge_tick.update_recipe
+    from conda_forge_tick.feedstock_parser import load_feedstock
+    from conda_forge_tick.update_recipe import v1_recipe
+    from conda_forge_tick.update_recipe.version import update_version_feedstock_dir
+    from conda_forge_tick.utils import setup_logging
+
+    setup_logging()
+
+    name = feedstock_name.rsplit("-", 1)[0]
+    LOGGER.info("using feedstock name %s", name)
+
+    try:
+        LOGGER.info("computing feedstock attributes")
+        attrs = load_feedstock(name, {}, use_container=False)
+        LOGGER.info("feedstock attrs:\n%s\n", pprint.pformat(attrs))
+    except Exception:
+        LOGGER.exception("error while computing feedstock attributes!")
+        sys.exit(1)
+
+    try:
+        LOGGER.info(
+            "updating version %s -> %s",
+            attrs.get("version", "null"),
+            new_version,
+        )
+        updated, errors = update_version_feedstock_dir(
+            feedstock_name,
+            str(new_version),
+            use_container=False,
+        )
+        if errors or (not updated):
+            raise RuntimeError(f"Error updating the recipe version:\n{errors!r}")
+    except Exception:
+        LOGGER.exception("error while updating the recipe version!")
+        sys.exit(1)
+
+    try:
+        LOGGER.info(
+            "resetting the build number",
+        )
+        workdir = Path(feedstock_name)
+        meta_yaml_path = workdir.joinpath("recipe", "meta.yaml")
+        recipe_yaml_path = workdir.joinpath("recipe", "recipe.yaml")
+        if meta_yaml_path.exists():
+            new_meta_yaml = meta_yaml_path.read_text()
+            new_meta_yaml = conda_forge_tick.update_recipe.update_build_number(
+                new_meta_yaml,
+                0,
+            )
+            meta_yaml_path.write_text(new_meta_yaml)
+        elif recipe_yaml_path.exists():
+            new_recipe_yaml = v1_recipe.update_build_number(
+                recipe_yaml_path,
+                0,
+            )
+            recipe_yaml_path.write_text(new_recipe_yaml)
+        else:
+            raise FileNotFoundError("Could not find meta.yaml or recipe.yaml!")
+    except Exception:
+        LOGGER.exception("error while resetting the recipe build number!")
+        sys.exit(1)
+
+    try:
+        LOGGER.info(
+            "rerendering",
+        )
+        msg = cf_feedstock_ops_rerender(
+            feedstock_name,
+            timeout=None,
+            use_container=False,
+        )
+        if msg is not None:
+            msg = "chore: update version to {new_version} & " + msg[len("chore: ") :]
+        print(f"commit-message={msg}" >> "$GITHUB_OUTPUT", flush=True)
+
+    except Exception:
+        LOGGER.exception("error while rerendering!")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
